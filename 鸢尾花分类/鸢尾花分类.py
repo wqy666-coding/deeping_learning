184e97132e42c0410df5397b9651b8784c57870f
wqy666-coding <2513959331@qq.com>
2024-03-27 21:10:25 +0800

    来了

---
+++ 鸢尾花分类/鸢尾花分类.py
@@ -0,0 +1,8 @@
+# 导入必要的包
+import numpy as np
+from matplotlib import colors
+from sklearn import svm
+from sklearn.svm import SVC
+from sklearn import model_selection
+import matplotlib.pyplot as plt
+import matplotlib as mpl
